eda62b31dffe917a7f31c215ba29c7fc6d6cb07a
Polanc <up4472@student.uni-lj.si>
2021-05-20 16:39:30 +0200

    Commit #015

--- run_bert.bat
+++
@@ -1,1 +0,0 @@
-python main.py --model BERT --extractor none --target CodePreliminary Topic "Book ID" --demo 4 --save 0 --verbose 1
--- run_classifiers.bat
+++
@@ -1,1 +0,0 @@
-python main.py --model LR RF NB KNN MV --extractor tfidf countvec handcrafted --target CodePreliminary Topic "Book ID" --demo 4 --save 0 --verbose 1
